977cd6ed37b2340945946a337596aad2bc295627
AlgoClassWork <kyxec.play@gmail.com>
2025-06-30 15:36:35 +0600

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,18 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, img, x, y, w, h):
+        super().__init__()
+        self.image = transform.scale(image.load(img), (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = GameSprite(img='player.png', x=300, y=400, w=100, h=100)
+
 window = display.set_mode( (700,500) )
 display.set_caption('Шутер')
 
@@ -11,5 +24,6 @@ while True:
             exit()
 
     window.blit( background, (0, 0) )
+    player.show()
 
     display.update()
