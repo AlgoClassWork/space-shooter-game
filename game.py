81e243b69a37d491acf8bf999b4885643fc15eac
AlgoClassWork <kyxec.play@gmail.com>
2025-07-01 14:41:28 +0600

    Update game.py

--- game.py
+++ game.py
@@ -18,16 +18,25 @@ class Player(GameSprite):
         mouse_x, mouse_y = mouse.get_pos() 
         player.rect.centerx = mouse_x 
 
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += randint(1, 5)
+        if self.rect.y > 500:
+            self.rect.x = randint(0, 600)
+            self.rect.y = 0
+
 player = Player(img='player.png', x=300, y=400, w=100, h=100)
 
 enemys = sprite.Group()
 for i in range(5):
     rand_x = randint(0, 600)
-    enemy = GameSprite(img='enemy.png', x=rand_x, y=0, w=100, h=50)
+    enemy = Enemy(img='enemy.png', x=rand_x, y=0, w=100, h=50)
     enemys.add(enemy)
 
 window = display.set_mode( (700,500) )
 display.set_caption('Шутер')
+
+clock = time.Clock()
 
 background =  transform.scale(image.load('background.jpg'), (700,500))
 
@@ -41,5 +50,7 @@ while True:
     player.move()
 
     enemys.draw(window)
+    enemys.update()
 
     display.update()
+    clock.tick(100)
